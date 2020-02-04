cfc81d191bc9f198651852103e5c28f8f3ca81c9
MFAC <ninafroes@gmail.com>
2020-02-04 17:54:24 +0100

    styles: Add Nav component styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,85 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  font-size: 1rem;
+  font-weight: bold;
+  border: 2px solid red;
+`
+
+const StyledUl = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  border: 2px solid blue;
+`
+
+const StyledLi = styled.li`
+  list-style-type: none;
+  color: "red";
+  padding: 10px;
+  border: 2px solid green;
+`
+
+const StyledImg = styled.img`
+  border-radius: 50%;
+`
+
+const GreetingSpan = styled.span`
+  font-weight: normal;
+  padding: 10px;
+`
+
+const UserDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid orange;
+  padding: 10px;
+`
+
+const StyledButton = styled.button`
+  background-color: grey;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  margin: 10px;
+`
 
 function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/" exact activeClassName="active">
+    <StyledNav>
+      <StyledUl>
+        <StyledLi>
+          <Link to="/" exact>
             Home
           </Link>
-          <Link to="/add" exact activeClassName="active">
+        </StyledLi>
+        <StyledLi>
+          <Link to="/add" exact>
             New Question
           </Link>
-          <Link to="/leaderboard" exact activeClassName="active">
+        </StyledLi>
+        <StyledLi>
+          <Link to="/leaderboard" exact>
             Leader Board
           </Link>
-        </li>
-      </ul>
-    </nav>
+        </StyledLi>
+      </StyledUl>
+      <UserDiv>
+        <GreetingSpan>Hello, UserName!</GreetingSpan>
+        <StyledImg
+          src="https://picsum.photos/seed/picsum/50/50"
+          alt="Avatar of UserName"
+        />
+        <StyledButton>
+          Logout
+        </StyledButton>
+      </UserDiv>
+    </StyledNav>
   )
 }
 
