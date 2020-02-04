b76ed62b23e1e4df29fdd1c9ffd0b334ba76a4d6
MFAC <ninafroes@gmail.com>
2020-02-04 17:08:51 +0100

    feat: Add Link to components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <div>
-      NAV
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" exact activeClassName="active">
+            Home
+          </Link>
+          <Link to="/add" exact activeClassName="active">
+            New Question
+          </Link>
+          <Link to="/leaderboard" exact activeClassName="active">
+            Leader Board
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
