47807ff8f9189ce80801aa9ef525fbeed4836e7c
MFAC <ninafroes@gmail.com>
2020-02-04 16:18:43 +0100

    feat: Import basic components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import '../App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './Home'
+import LeaderBoard from './LeaderBoard'
+// import Login from './Login'
+import Nav from './Nav'
+import NewQuestion from './NewQuestion'
+import QuestionPage from './QuestionPage'
 
 function App() {
   return (
-    <div className="App">
-      APP
-    </div>
-  );
+    <Router>
+      <Fragment>
+        <Nav />
+        <div className="App">
+          <Route to="/" exact component={Home} />
+          <Route to="/add" component={NewQuestion} />
+          <Route to="/leaderboard" component={LeaderBoard} />
+          <Route to="/question/:question_id" component={QuestionPage} />
+        </div>
+      </Fragment>
+    </Router>
+  )
 }
 
 export default App;
