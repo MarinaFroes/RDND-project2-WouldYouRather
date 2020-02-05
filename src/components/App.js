9f0b05ff1f7ef196950198871684f0fee640186b
MFAC <ninafroes@gmail.com>
2020-02-05 16:22:14 +0100

    feat: Add mapStateToProps and connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import '../App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import Home from './Home'
 import LeaderBoard from './LeaderBoard'
@@ -8,21 +9,40 @@ import LeaderBoard from './LeaderBoard'
 import Nav from './Nav'
 import NewQuestion from './NewQuestion'
 import QuestionPage from './QuestionPage'
+import { handleInitialData } from '../actions/shared'
 
-function App() {
-  return (
-    <Router>
-      <Fragment>
-        <Nav />
-        <div className="App">
-          <Route path="/" exact component={Home} />
-          <Route path="/add" exact component={NewQuestion} />
-          <Route path="/leaderboard" exact component={LeaderBoard} />
-          <Route path="/question/:question_id" component={QuestionPage} />
-        </div>
-      </Fragment>
-    </Router>
-  )
+class App extends Component() {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <Router>
+        <Fragment>
+          <Nav userName={"Marina"} />
+          {this.props.loading === true ? null : (
+            <div className="App">
+              <Route path="/" exact component={Home} />
+              <Route path="/add" exact component={NewQuestion} />
+              <Route path="/leaderboard" exact component={LeaderBoard} />
+              <Route path="/question/:question_id" component={QuestionPage} />
+            </div>
+          )}
+        </Fragment>
+      </Router>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
+
+
+
