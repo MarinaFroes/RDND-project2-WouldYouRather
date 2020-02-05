6fa31fd48e7c46a3b384e5f2ebdc09703fbb5470
MFAC <ninafroes@gmail.com>
2020-02-05 23:10:19 +0100

    feat: Add connect and mapStateToProps

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 
 const StyledImg = styled.img`
   border-radius: 50%;
   padding: 0 20px;
+  height: 100px;
 `
 
 const StyledContainer = styled.div`
   border: 2px solid #e8e9eb;
   border-radius: 5px;
   width: 50%;
+  margin-top: 2rem;
 `
 
 const QuestionDiv = styled.div`
@@ -19,35 +22,91 @@ const QuestionDiv = styled.div`
 const Title = styled.div`
   display: flex;
   border-bottom: 2px solid #e8e9eb;
+  padding-left: 20px;
+  background-color: #e6e6e6;
 `
 
 const Button = styled.button`
-  background-color: #3283a8;
-  color: #f0f0f0;
+  background-color: #e6e6e6;
+  color: #000;
   width: 70%;
   margin: 2rem;
+  border: none;
+  border-radius: 5px;
+  padding: 3px;
+  font-weight: bold;
+  &:hover {
+    background-color: #bfbfbf;
+    cursor: pointer;
+  }
+`
+const StyledDiv = styled.div`
+  display: flex;
+`
+const UserDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-right: 2px solid #e6e6e6;
+  margin: 20px 0;
 `
 
-function Question({userName, userId, avatarURL, optionOne, optionTwo}) {
-  return (
-    <StyledContainer>
-      <Title>
-        <StyledImg src={avatarURL} alt={`Avatar de ${userName}`} />
-        <p>{`${userName} asks:`}</p>
-      </Title>
-      <QuestionDiv>
-        <h3>Would you rather...</h3>
-        <form onSubmit={e => {
-          e.preventDefault()
-          console.log(e.target.value)
-        }}>
-          <input type="radio" name="option" value="optionOne" />{optionOne}<br/>
-          <input type="radio" name="option" value="optionTwo" />{optionTwo}<br/>
-          <Button type="submit">Submit</Button>
-        </form>
-      </QuestionDiv>
-    </StyledContainer>
-  )
+class Question extends Component {
+  render(){
+    const { avatarURL, userName, question, answered } = this.props
+    
+    return (
+      <StyledContainer id={question.id}>
+        <Title>
+          <p>{`${userName} asks:`}</p>
+        </Title>
+        <StyledDiv>
+          <UserDiv>
+            <StyledImg src={avatarURL} alt={`Avatar of ${userName}`} />
+          </UserDiv>
+          <QuestionDiv>
+            <h3>Would you rather...</h3>
+            {answered.includes(question.id) ? (
+              <div>
+                <p>
+                  {question.optionOne.text} or {question.optionTwo.text}?
+                </p>
+                <button>Check results</button>
+              </div>
+            ) : (
+              <form
+                onSubmit={e => {
+                  e.preventDefault()
+                  console.log(e.target.value)
+                }}
+              >
+                <input type="radio" name="option" value="optionOne" />
+                {question.optionOne.text}
+                <br />
+                <input type="radio" name="option" value="optionTwo" />
+                {question.optionTwo.text}
+                <br />
+                <Button type="submit">Submit</Button>
+              </form>
+            )}
+          </QuestionDiv>
+        </StyledDiv>
+      </StyledContainer>
+    )
+  }
 }
 
-export default Question
+function mapStateToProps({ authedUser, users, questions }, { qid }) {
+  const question = questions[qid]
+  const userId = question.author
+  const answered = Object.keys(users[authedUser].answers)
+  
+  return {
+    question,
+    avatarURL: users[userId].avatarURL,
+    userName: users[userId].name,
+    answered
+  }
+}
+
+export default connect(mapStateToProps)(Question)
