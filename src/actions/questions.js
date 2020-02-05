c198182311d33bc2ac3445290e8b565a4a9a1ffc
MFAC <ninafroes@gmail.com>
2020-02-05 21:58:42 +0100

    refactor: Change double quotes for single quotes

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,5 +1,5 @@
 import { showLoading, hideLoading } from 'react-redux-loading'
-import { saveQuestion, saveQuestionAnswer } from "../utils/api"
+import { saveQuestion, saveQuestionAnswer } from '../utils/api'
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 export const ADD_QUESTION = 'ADD_QUESTION'
