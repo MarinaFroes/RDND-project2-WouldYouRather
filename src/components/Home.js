6ffaf2895e72d9f096bc25fd669a9e635544d06f
MFAC <ninafroes@gmail.com>
2020-02-05 23:10:41 +0100

    feat: Add connect and mapStateToProps

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
+
+import Question from './Question'
+
+const StyledMain = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        HOME
-      </div>
+      <StyledMain>
+        {this.props.questionIds.map(qid => <Question key={qid} qid={qid} />)}
+      </StyledMain>
     )
   }
 }
 
-export default Home
+
+function mapStateToProps({ questions }) {
+  return {
+    questionIds: Object.keys(questions)
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Home)
