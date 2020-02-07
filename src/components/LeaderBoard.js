3e871f5f982136d1241e45e3d25a02ed68585736
MFAC <ninafroes@gmail.com>
2020-02-07 22:14:53 +0100

    feat: Add hard coded LeaderBoard content

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import User from './User'
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
 
 class LeaderBoard extends Component {
   render() {
     return (
-      <div>
-        LEADER BOARD
-      </div>
+      <Main>
+        <h2>Leader Board</h2>
+        <User
+          position={1}
+          userName="Sarah Edo"
+          avatarURL="https://picsum.photos/id/1027/200/200"
+          questions={10}
+          answers={5}
+          score={10 + 5}
+        />
+        <User
+          position={1}
+          userName="Sarah Edo"
+          avatarURL="https://picsum.photos/id/1027/200/200"
+          questions={10}
+          answers={5}
+          score={10 + 5}
+        />
+        <User
+          position={1}
+          userName="Sarah Edo"
+          avatarURL="https://picsum.photos/id/1027/200/200"
+          questions={10}
+          answers={5}
+          score={10 + 5}
+        />
+      </Main>
     )
   }
 }
