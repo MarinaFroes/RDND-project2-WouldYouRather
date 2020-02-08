080eada2f162556d599e32e6336f575eb2d6462f
MFAC <ninafroes@gmail.com>
2020-02-08 18:39:15 +0100

    feat: Add QuestionPreview component

---
+++ src/components/QuestionPreview.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const StyledLink = styled(Link)`
+  border: none;
+  padding: 5px;
+  text-align: center;
+  &:hover {
+    font-weight: bold;
+    cursor: pointer;
+  }
+`
+function QuestionPreview({ optionOneText, optionTwoText, id, isAnswered }) {
+  return (
+    <div>
+      <p>
+        ...{optionOneText} or {optionTwoText}?
+      </p>
+      <StyledLink to={`/question/${id}`}>
+        {
+          isAnswered === true 
+          ? 'Click here to check Answers' 
+          : 'Click here to answer'
+        }
+      </StyledLink>
+    </div>
+  )
+}
+
+export default QuestionPreview
