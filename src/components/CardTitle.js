103a082569c2908bd1194c8bae44307c797d32bc
MFAC <ninafroes@gmail.com>
2020-02-09 16:01:12 +0100

    feat: Add date of creation

--- src/components/CardTitle.js
+++ src/components/CardTitle.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import { formatDate } from '../utils/helpers'
 
 const Title = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
   border-bottom: 2px solid #e8e9eb;
-  padding-left: 20px;
+  padding: 0 20px;
   background-color: ${props => props.titleColor || "#ccedd5"};
 `
 
-function CardTitle({ titleColor, text }) {
+const Date = styled.p`
+  font-size: 0.8rem;
+  color: #535454;
+`
+const StyledText = styled.p`
+  font-zise: 1.2rem;
+  font-weight: bold;
+`
+
+function CardTitle({ titleColor, text, timestamp }) {
   return (
-    <Title titleColor={titleColor}>
-      <p>{text}</p>
+    <Title titleColor={titleColor} timestamp={timestamp}>
+      <StyledText>{text}</StyledText>
+      {timestamp && <Date>Created on: {formatDate(timestamp)}</Date>}
     </Title>
   )
 }
