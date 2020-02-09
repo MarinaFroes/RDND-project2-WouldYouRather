2307122a72570b2427c1ae01c69eaaac2b0ad519
MFAC <ninafroes@gmail.com>
2020-02-09 12:02:45 +0100

    feat: Add updateUsers and handleUpdateUsers action creators

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,4 +1,8 @@
+import { getUsers } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const UPDATE_USERS = 'UPDATE_USERS'
 
 export function receiveUsers(users) {
   return {
@@ -6,3 +10,33 @@ export function receiveUsers(users) {
     users
   }
 }
+
+export function updateUsers(users) {
+  return {
+    type: UPDATE_USERS,
+    users
+  }
+}
+
+export function handleUpdateUsers() {
+  return dispatch => {
+    dispatch(showLoading())
+    return getUsers().then((users) => {
+      dispatch(updateUsers(users))
+      dispatch(hideLoading())
+    })
+  }
+}
+/**
+ * export function handleInitialData() {
+  return dispatch => {
+    dispatch(showLoading())
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(receiveUsers(users))
+      dispatch(receiveQuestions(questions))
+      dispatch(setAuthedUser(AUTHED_ID))
+      dispatch(hideLoading())
+    })
+  }
+}
+ */
