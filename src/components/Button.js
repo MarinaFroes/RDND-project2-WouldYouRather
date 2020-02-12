8ac22a2ee8b8578d4cde05b4657f7d463f9ff357
MFAC <ninafroes@gmail.com>
2020-02-12 21:32:24 +0100

    feat: Create Button reusable component

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  background-color: #e6e6e6;
+  color: #000;
+  margin: 2rem;
+  width: 60%;
+  border: none;
+  padding: 5px;
+  font-weight: bold;
+  font-size: 1rem;
+  text-decoration: none;
+  &:hover {
+    background-color: #bfbfbf;
+    cursor: pointer;
+  }
+`
+
+function Button() {
+  return (
+    <StyledButton type="submit">
+      Submit
+    </StyledButton>
+  )
+}
+
+export default Button
