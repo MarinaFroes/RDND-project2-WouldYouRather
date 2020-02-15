c2fa10b642b3ff5e881d1fc796f1e940166b234b
MFAC <ninafroes@gmail.com>
2020-02-15 19:33:51 +0100

    feat: Add content to NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FaRegSadTear } from 'react-icons/fa'
 
-export default function NotFound() {
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #586b5d;
+`
+
+const StyledIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #586b5d;
+  font-size: 4rem;
+  margin: 2rem;
+`
+
+const StyledH1 = styled.h1`
+  font-size: 5rem;
+`
+
+const Text = styled.p`
+  font-size: 1.5rem;
+  margin: 0.5rem;
+  text-align: center;
+`
+
+function NotFound() {
   return (
-    <div>
-      <p>NOT FOUND</p>
-    </div>
+    <>
+      <StyledDiv>
+        <StyledIcon>{<FaRegSadTear />}</StyledIcon>
+        <StyledH1>404</StyledH1>
+      </StyledDiv>
+      <Text>Sorry, this page doesn't exist</Text>
+      <Text>Go back to <a href="/">Home</a> page</Text>
+    </>
   )
 }
+
+export default NotFound
