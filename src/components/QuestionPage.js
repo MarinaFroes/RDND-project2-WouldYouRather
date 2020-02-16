d2d452bc3b1e5dac345cb5498f240850f7da5089
MFAC <ninafroes@gmail.com>
2020-02-16 08:48:07 +0100

    refactor: Change class component to functional component

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 import QuestionDetails from './QuestionDetails'
@@ -12,20 +12,20 @@ const ErrorMessage = styled.h1`
   color: red;
 `
 
-class QuestionPage extends Component {
-  render() {
-    const id = this.props.match.params.question_id
-    
-    return (
-      <StyledDiv>
-        {id !== undefined ? (
-          <QuestionDetails qid={id} page="QuestionPage" />
-        ) : (
-          <ErrorMessage>Question not found</ErrorMessage>
-        )}
-      </StyledDiv>
-    )
-  }
+function QuestionPage (props) {
+  
+  const id = props.match.params.question_id
+  
+  return (
+    <StyledDiv>
+      {id !== undefined ? (
+        <QuestionDetails qid={id} page="QuestionPage" />
+      ) : (
+        <ErrorMessage>Question not found</ErrorMessage>
+      )}
+    </StyledDiv>
+  )
+  
 }
 
 export default QuestionPage
